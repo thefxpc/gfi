b952779e5dc9c0e5141e7e789e1a428d7f577433
Xavier <jperez@oeste.mx>
2020-07-24 17:07:37 -0500

    Añadidos algunos datos de prueba

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,7 @@ INSERT INTO cat_tipo_metal VALUES(3,'Death');
 INSERT INTO cat_tipo_metal VALUES(4,'Black');
 INSERT INTO cat_tipo_metal VALUES(5,'Power');
 INSERT INTO cat_tipo_metal VALUES(6,'Vegetariano progresivo');
+
+
+INSERT INTO banda(banda_id,nombre,fecha_fundacion,disco_lanzados,metal_tipo_id)
+VALUES (3000,'Porcupine Tree',CURRENT_TIMESTAMP(),10,6)
